c4261e45636044558cc7a208e858eef0d9b169e7
Hayato Ito <hayato@gclue.jp>
2012-12-18 12:28:53 +0900

    初コミット

---
+++ OrangeMathLib/src/com/existmikan/orange/math/Main.java
@@ -0,0 +1,13 @@
+package com.existmikan.orange.math;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
